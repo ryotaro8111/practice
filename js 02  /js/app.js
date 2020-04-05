8fa1d525c389e01a170027591ec15b1e51c23475
ryotaro8111 <ryo8111@kinoshitaryoutarounoMacBook-ea.local>
2020-04-05 10:56:36 +0900

    条件分岐の練習

---
+++ js 02  /js/app.js
@@ -0,0 +1,15 @@
+// const age = window.prompt("年齢を入力してください");
+// if ( age >= 20 ) {
+//   document.write("20才以上なのでお酒が買えます");
+// } else {
+//   document.write ("20才以下なのでお酒は買えません");
+// }
+
+// const diet =window.prompt("あなたの体重は？");
+// if ( diet <= 68 ) {
+//   document.write("すごい痩せましたね！！");
+// } else if ( diet >=68 && diet <= 75 ) {
+//   document.write("あと少し頑張って！");
+// } else {
+//  document.write(" まだまだ頑張りましょう");
+// }
