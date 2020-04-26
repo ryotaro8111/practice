c2b56329d608dd16c0000d68f38cab45a20918e7
ryotaro8111 <ryo8111@kinoshitaryoutarounoMacBook-ea.local>
2020-04-26 14:30:23 +0900

    配列の練習

---
+++ js 02  practice /js/app.js
@@ -0,0 +1,28 @@
+const age = window.prompt("年齢を入力してください");
+if ( age >= 20 ) {
+  document.write("20才以上なのでお酒が買えます");
+} else {
+  document.write ("20才以下なのでお酒は買えません");
+}
+
+// const diet =window.prompt("あなたの体重は？");
+// if ( diet <= 68 ) {
+//   document.write("すごい痩せましたね！！");
+// } else if ( diet >=68 && diet <= 75 ) {
+//   document.write("あと少し頑張って！");
+// } else {
+//  document.write(" まだまだ頑張りましょう");
+// }
+
+// const fuku = window.prompt("1〜10の数字を入力してください");
+// switch (fuku) {
+//   case "5":
+//     document.write("1等です！旅行が当たりました");
+//     break;
+//   case "3":
+//     document.write("2等です！100インチテレビが当たりました");
+//     break;
+//   default:
+//     document.write("外れ！ティッシュをあげます");
+//     break;
+// }
---
+++ js03/js/app.js
@@ -0,0 +1,15 @@
+// const menbers = ["田中","高橋","鈴木","木下","南地"];
+// document.write(menbers[2]);
+// 配列のながさを調べる
+// document.write(menbers.length);
+
+// menbers.push("山田");
+// document.write(menbers.length);
+// document.write(menbers[5]);
+
+const members = {
+  name:"田中",
+  age:20,
+  gender:"女性",
+}
+document.write(members.age);
